039e7009dade9c4c317d64fef0bd5e5a30925bc5
soieu <soieu9898@gmail.com>
2023-10-28 20:06:50 +0900

    fix: api-config.js fix

--- src/api-config.js
+++ src/api-config.js
@@ -3,11 +3,9 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  if (hostname === "localhost") {
-    backendHost = "localhost:8080";
-  } else {
-    backendHost = "http://todo-api-soieu.ap-northeast-2.elasticbeanstalk.com";
-  }
+  backendHost = "localhost:8080";
+} else {
+  backendHost = "http://todo-api-soieu.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
