47706d754a480df07d9a09410b1c9af5113a4b71
soieu <soieu9898@gmail.com>
2023-10-28 19:37:04 +0900

    commit for deployment

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,11 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://localhost:8080";
+  if (hostname === "localhost") {
+    backendHost = "localhost:8080";
+  } else {
+    backendHost = "http://todo-api-soieu.ap-northeast-2.elasticbeanstalk.com";
+  }
 }
 
 export const API_BASE_URL = `${backendHost}`;
